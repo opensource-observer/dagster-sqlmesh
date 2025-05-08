b166bbee46826875551ba2632fb1dce5431d53d9
Luis Carbonell Girones <3169189+lucargir@users.noreply.github.com>
2025-05-08 09:45:01 +0200

    fix: undo singleton for translator

--- dagster_sqlmesh/asset.py
+++ dagster_sqlmesh/asset.py
@@ -22,7 +22,7 @@ def sqlmesh_assets(
     config: SQLMeshContextConfig,
     context_factory: ContextFactory[ContextCls] = lambda **kwargs: Context(**kwargs),
     name: str | None = None,
-    dagster_sqlmesh_translator: SQLMeshDagsterTranslator = SQLMeshDagsterTranslator(),
+    dagster_sqlmesh_translator: SQLMeshDagsterTranslator | None = None,
     compute_kind: str = "sqlmesh",
     op_tags: t.Mapping[str, t.Any] | None = None,
     required_resource_keys: set[str] | None = None,
@@ -31,7 +31,8 @@ def sqlmesh_assets(
     enabled_subsetting: bool = False,
 ) -> t.Callable[[t.Callable[..., t.Any]], AssetsDefinition]:
     controller = DagsterSQLMeshController.setup_with_config(config=config, context_factory=context_factory)
-    
+    if not dagster_sqlmesh_translator:
+        dagster_sqlmesh_translator = SQLMeshDagsterTranslator()
     conversion = controller.to_asset_outs(environment, translator=dagster_sqlmesh_translator)
 
     return multi_asset(
